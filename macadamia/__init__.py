35d10557d02eea91a78bea2348298a27b5afe768
Sac <sacha@ripio.com>
2018-11-07 15:41:38 -0300

    fix lib for python3

--- macadamia/__init__.py
+++ macadamia/__init__.py
@@ -1,19 +1,22 @@
 import datetime
-import urllib2
+import urllib.parse
+
 
 def parse_campaign_data(data):
     human_names = {
-      "utmcsr": "source",
-      "utmcmd": "medium",
-      "utmccn": "campaign_name",
-      "utmctr": "campaign_keyword",
-      "utmcct": "campaign_content",
-      "utmgclid": "google_click_id",
+        "utmcsr": "source",
+        "utmcmd": "medium",
+        "utmccn": "campaign_name",
+        "utmctr": "campaign_keyword",
+        "utmcct": "campaign_content",
+        "utmgclid": "google_click_id",
     }
+
     fields = [d.split("=") for d in data.split("|")]
-    info = dict((human_names[d[0]] ,urllib2.unquote(d[1])) for d in fields)
+    info = dict((human_names[d[0]], urllib.parse.unquote(d[1])) for d in fields)
 
     return info
+
 
 def parse_utmz(cookie):
     fields = cookie.split('.')
